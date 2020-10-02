9cad7f7ef31bc1aa8b73131439d2ecb31fd0d33e
Benjamin <nimajneb2005@gmx.de>
2020-10-02 13:52:17 +0200

    Commit to master

--- generate.py
+++ generate.py
@@ -3,16 +3,22 @@ import sys
 import markdown
 import argparse
 import os
+import re
 
 class Generator():
 
     def __init__(self):
         self.CWD = os.getcwd()
 
+    def debug(self, text: str):
+        if self.args.debug:
+            print(text)
+
     def checkArgs(self):
         parser = argparse.ArgumentParser(description='Create a static website from Markdown or HTML files.')
         parser.add_argument('inputfolder', type=str, help='Folder with the files the website should be created from')
-        parser.add_argument('-o', '--outputfolder', type=str, help='Folder where the finished website should be dropped')
+        parser.add_argument('outputfolder', type=str, help='Folder where the finished website should be dropped')
+        parser.add_argument('--markdown', type=bool, help='Pages are getting converted from Markdown to HTML before assembly')
         parser.add_argument('--debug', type=bool, help='Prints debug info')
         self.args = parser.parse_args()
         
@@ -20,13 +26,47 @@ class Generator():
         if not os.path.exists(self.args.inputfolder):
             print("Inputfolder doesn't exists")
             sys.exit(2)
+        
+        # Check if the outputfolder exists
+        if not os.path.exists(self.args.outputfolder):
+            print("Outputfolder doesn't exists")
+            sys.exit(2)
+        
+        self.debug('Args checked')
+        
 
     def findFiles(self):
         self.FILES = [i if os.path.isfile(os.path.join(self.CWD, self.args.inputfolder, i)) else [i, os.listdir(os.path.join(self.CWD, self.args.inputfolder, i))] for i in os.listdir(os.path.join(self.CWD, self.args.inputfolder))]
+        self.debug(f'Files: {self.FILES}')
+
+    def loadHeaderAndFooter(self):
+        with open(os.path.join(self.CWD, self.args.inputfolder, '_header.html')) as f:
+            self.HEADER = f.read()
+        
+        with open(os.path.join(self.CWD, self.args.inputfolder, '_footer.html')) as f:
+            self.FOOTER = f.read()
+
+    def createNav(self):
+        nav = ''
+        for item in self.FILES:
+            if type(item) == str and item != '_header.html' and item != '_footer.html':
+                nav += f'<a class="nav_link" href="{item}">{item}</a>'
+
+        self.HEADER = self.HEADER.replace('<Links />', nav)
 
     def generate(self):
         self.checkArgs()
         self.findFiles()
+        self.loadHeaderAndFooter()
+        self.createNav()
+        for file in self.FILES:
+            if type(file) == str and file != '_header.html' and file != '_footer.html':
+                if self.args.markdown:
+                    pass
+                
+            elif type(file) == list:
+                pass
+        self.debug(f'{self.HEADER}\n{self.FOOTER}')
 
 
 if __name__ == "__main__":
