627d88097bd8249f06bba6c6b2141e604f1f1ee0
Benjamin <nimajneb2005@gmx.de>
2020-10-03 13:45:13 +0200

    Create <head> and links for the <nav>

--- generate.py
+++ generate.py
@@ -3,7 +3,6 @@ import sys
 import markdown
 import argparse
 import os
-import re
 
 class Generator():
 
@@ -18,6 +17,7 @@ class Generator():
         parser = argparse.ArgumentParser(description='Create a static website from Markdown or HTML files.')
         parser.add_argument('inputfolder', type=str, help='Folder with the files the website should be created from')
         parser.add_argument('outputfolder', type=str, help='Folder where the finished website should be dropped')
+        parser.add_argument('-s', '--stylesheet', type=str, help='Stylesheet that should be used for the page')
         parser.add_argument('--markdown', type=bool, help='Pages are getting converted from Markdown to HTML before assembly')
         parser.add_argument('--debug', type=bool, help='Prints debug info')
         self.args = parser.parse_args()
@@ -40,23 +40,42 @@ class Generator():
         self.debug(f'Files: {self.FILES}')
 
     def loadHeaderAndFooter(self):
+        # Open <header>
         with open(os.path.join(self.CWD, self.args.inputfolder, '_header.html')) as f:
             self.HEADER = f.read()
         
+        # Open <footer>
         with open(os.path.join(self.CWD, self.args.inputfolder, '_footer.html')) as f:
             self.FOOTER = f.read()
+    
+    def createHead(self):
+        # Read the basic <head> from file
+        with open(os.path.join(self.CWD, self.args.inputfolder, '_head.html')) as f:
+            BASIC_HEAD = f.read()
+        
+        # Stylesheet
+        if self.args.stylesheet != None:
+            STYLESHEET = f'<link rel="stylesheet" href="{self.args.stylesheet}">'
+        
+        # Assembly
+        self.HEAD = f'{BASIC_HEAD}{STYLESHEET}'
 
     def createNav(self):
         nav = ''
+        FILE_EXTENTION = 'md' if self.args.markdown else 'html'
         for item in self.FILES:
-            if type(item) == str and item != '_header.html' and item != '_footer.html':
-                nav += f'<a class="nav_link" href="{item}">{item}</a>'
+            if type(item) == str and item != '_header.html' and item != '_footer.html' and FILE_EXTENTION == item[-len(FILE_EXTENTION):]:
+                title = item[:-(len(FILE_EXTENTION) + 1)] if item != f'index.{FILE_EXTENTION}' else 'Home'
+                nav += f'<a class="nav_link" href="{item}">{title}</a>'
+            elif type(item) == list and f'index.{FILE_EXTENTION}' in item[1]:
+                nav += f'<a class="nav_link" href="{item[0]}/index.{FILE_EXTENTION}">{item[0]}</a>'
 
-        self.HEADER = self.HEADER.replace('<Links />', nav)
+        self.HEADER = self.HEADER.replace('<AutoNav />', nav)
 
     def generate(self):
         self.checkArgs()
         self.findFiles()
+        self.createHead()
         self.loadHeaderAndFooter()
         self.createNav()
         for file in self.FILES:
